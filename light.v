2335cffc3b88da9d8f1468fe18476346ac723d97
Michelle Joo <michellejoo@u.boisestate.edu>
2026-02-25 18:33:30 -0700

    Update light.v

--- light.v
+++ light.v
@@ -1,8 +1,12 @@
 module light(
     // Declare downstairs and upstairs input
+    input wire downstairs,
+    input wire upstairs,
     // Declare stair light output
+    output wire stair_light
 );
 
     // Enter logic equation here
+    assign stair_light = downstairs ^ upstairs;
 
 endmodule
