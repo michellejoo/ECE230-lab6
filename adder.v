ca7e95ddffebc9a0275732b0d031f597dc347bb4
Michelle Joo <michellejoo@u.boisestate.edu>
2026-02-25 18:34:44 -0700

    Update adder.v

--- adder.v
+++ adder.v
@@ -1,9 +1,16 @@
 module adder(
     // Declare your A/B inputs
+    input wire A,
+    input wire B,
     // Declare Y output
+    output wire Y,
     // Declare carry output
+    output wire carry
 );
 
     // Enter logic equation here
+    // Half-adder eq
+    assign Y = A ^ B;
+    assign carry = A & B;
 
 endmodule
